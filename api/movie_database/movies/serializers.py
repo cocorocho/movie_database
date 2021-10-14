b085bb2dcefba8af70cd82f222c65ea68bf303af
cocorocho <cobanerkan@yahoo.com>
2021-10-14 22:11:20 +0300

    add poster serializer

--- api/movie_database/movies/serializers.py
+++ api/movie_database/movies/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Movie, Genre
+from .models import Language, Movie, Genre, Poster
 
 
 class GenreSerializer(serializers.ModelSerializer):
@@ -8,9 +8,22 @@ class GenreSerializer(serializers.ModelSerializer):
         fields = ["name"]
 
 
+class PosterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Poster
+        fields = ["path"]
+
+
+class LanguageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Language
+        fields = ["name"]
+
+
 class MovieSerializer(serializers.ModelSerializer):
-    director = serializers.CharField(source="director.name")
     genres = GenreSerializer(read_only=True, many=True)
+    poster = PosterSerializer(read_only=True, many=False)
+    language = LanguageSerializer(read_only=True, many=False)
 
     class Meta:
         model = Movie
