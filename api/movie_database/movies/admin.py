4375850d95c912a98082ebad8dc088cf86b5059e
cocorocho <cobanerkan@yahoo.com>
2021-10-14 22:11:12 +0300

    add poster to admin view

--- api/movie_database/movies/admin.py
+++ api/movie_database/movies/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Genre, Director, Movie
+from .models import Genre, Director, Movie, Poster
 
 # Register your models here.
-admin.site.register([Genre, Director, Movie])
+admin.site.register([Genre, Director, Movie, Poster])
