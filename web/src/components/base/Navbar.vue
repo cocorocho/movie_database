140d3e163980fcd066e4119d1f0ba2dff221a9ad
Erkan <cobanerkan@yahoo.com>
2022-02-04 18:26:55 +0300

    add logo

--- web/src/components/base/Navbar.vue
+++ web/src/components/base/Navbar.vue
@@ -5,10 +5,9 @@
 				<div class="flex justify-between">
 					<div class="flex space-x-7">
 						<div>
-							<!-- Website Logo -->
 							<a href="#" class="flex items-center py-4 px-2">
-								<img src="logo.png" alt="Logo" class="h-8 w-8 mr-2">
-								<span class="font-semibold text-gray-500 text-lg">Find Movie</span>
+								<img src="@/assets/video-player.png" alt="Logo" class="h-8 w-8 mr-2">
+								<span class="font-semibold text-white text-lg">Movie Database</span>
 							</a>
 						</div>
 						<!-- Primary Navbar items -->
